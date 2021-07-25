dabe6a1d1e549d6c0055777d30d86f218642d9c7
M Amin Nasiri <khodexenon@gmail.com>
2021-07-26 00:40:24 +0430

    http set to default

--- main/design/arg_options.py
+++ main/design/arg_options.py
@@ -20,14 +20,14 @@ def parse_args():
                                      '\n  GUI: python3 %(prog)s --gui')
     parser.add_argument('--gui', help='Start GUI Window', action='store_true')
     parser.add_argument('--web', help='Start Web Interface', action='store_true')
-    parser.add_argument('--no-https', help='Disable HTTPS for Web Interface', action='store_true')
+    parser.add_argument('--use-https', help='Enable HTTPS for Web Interface', action='store_true')
 
     args ,unknown = parser.parse_known_args()
 
     if (args.web):
-        use_ssl = True # True if --no-https arg is not set
-        if args.no_https:
-            use_ssl = None
+        use_ssl = False # False if --use-https arg is not set
+        if args.use_https:
+            use_ssl = True
         start_web(use_ssl=use_ssl)
     elif (args.gui):
         start_gui()
